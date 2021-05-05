fa0ed4bb9fb4dcc889d2b74a2a285999f0829586
Daniel Phan <sc20dkvp@leeds.ac.uk>
2021-05-05 20:21:12 +0100

    Completed the write and read state functions.

--- fileHandler.c
+++ fileHandler.c
@@ -1,11 +1,177 @@
 #include "fileHandler.h"
+#include <string.h>
 
+int iterations = 0;
+int tickRate = 0;
+
+//Returns 0 on successful file write.
+//Returns 1 on invalid file.
 int writeState(FILE *file)
 {
-    return 1;
+    //Checks if the file is open.
+    if(file == NULL)
+    {
+        return 1;
+    }
+
+    //Writes the width and the height of the grid on the first line.
+    fprintf(file,"%i/%i\n", grid.width, grid.height);
+
+    //Counts the number of dead and alive cells.
+    int countAlive = 0;
+    int countDead = 0;
+
+    //Loop to print out the cells state in the txt file.
+    for(int y = 0; y < grid.height; y++)
+    {
+        for(int x = 0; x < grid.width; x++)
+        {
+            if(grid.cells[y][x].state == true)
+            {
+                if(countDead != 0)
+                    fprintf(file, "%id", countDead);
+                countDead = 0;
+                countAlive++;
+            }
+            else
+            {
+                if(countAlive != 0)
+                    fprintf(file,"%ia", countAlive);
+                countAlive = 0;
+                countDead++;
+            }
+        }
+        if(countAlive != 0)
+            fprintf(file,"%ia", countAlive);
+        if(countDead != 0)
+            fprintf(file, "%id", countDead);
+        countAlive = 0;
+        countDead = 0;
+        fprintf(file, "\n");
+    }
+
+    return 0;
 }
 
+//Returns 0 on successful file read
+//Returns 1-9 based on different errors.
 int readState(FILE *file)
 {
-    return 1;
+    //Checks if the file is open.
+    if(file == NULL)
+    {
+        return 1;
+    }
+    //Temporary variables.
+    unsigned int count;
+    int height, width;
+    char temp1[20];
+    char temp2[20];
+    char *ptr;
+    char buffer[BUFFER_SIZE];
+
+    //Reads in the number of iterations to run through, and the tick rate of the game.
+    fgets(buffer, BUFFER_SIZE, file);
+    if(2 != sscanf(buffer, "%[^/]/%s", temp1, temp2))
+    {
+        return 2;
+    }
+    iterations = (int) strtol(temp1, &ptr, 10);
+    tickRate = (int) strtol(temp2, &ptr, 10);
+
+    //Return error messages 3 and 4 if the tick rate or number of iterations input are too high
+    if(iterations > 50)
+    {
+        return 3;
+    }
+    if((tickRate > 1000) || (tickRate < 200))
+    {
+        return 4;
+    }
+
+    //Reads in the width and height of the grid from the first line of the file.
+    //Converts to unsigned integer.
+    fgets(buffer, BUFFER_SIZE, file);
+    if(2 != sscanf(buffer, "%[^/]/%s", temp1, temp2))
+    {
+        return 2;
+    }
+    width = (int) strtol(temp1, &ptr,10);
+    height = (int) strtol(temp2, &ptr, 10);
+
+    //Checks if the input for the grid height and width is an integer.
+    if((height == 0) || (width == 0))
+    {
+        return 5;
+    }
+    createGrid(width, height);
+
+    //Reads in the lines containing the grid data.
+    fgets(buffer, BUFFER_SIZE, file);
+    for(int i = 0; i < height; i++)
+    {
+        //Removes the end line character
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        //total_n used to store the pointer location for 'd' and 'a', where the next sscanf will begin.
+        //n stores the end pointer location.
+        //Start variable used to determine which cell to begin setting.
+        //Count used to store the number of cells in a row with that state.
+        int total_n = 0;
+        int n;
+        int start = 0;
+        count = 0;
+
+        //While loop ending when there is nothing left to read in.
+        while (2 == sscanf(buffer + total_n, GAMEOFLIFE_FORMAT, &count, temp1, &n))
+        {
+            total_n += n;
+
+            //Returns 6 if the number of cells does not match the grid width.
+            if((start + count) > grid.width)
+            {
+                return 6;
+            }
+
+            //Sets the cells to dead if temp1 == 'd'
+            if(strcmp(temp1, "d") == 0)
+            {
+                for(int j = start; j < start + count; j++)
+                {
+                    //Returns 7 if out of bounds.
+                    if(setInitialState(j, i, false) == 1)
+                        return 7;
+                }
+            }
+            //Sets the cells to alive if temp1 == 'a'
+            else if(strcmp(temp1, "a") == 0)
+            {
+                for(int j = start; j < start + count; j++)
+                {
+                    //Returns 7 if out of bounds.
+                    if(setInitialState(j, i, true))
+                        return 7;
+                }
+            }
+            //Returns 8 for invalid file format. ie. Characters other than 'd' or 'a'
+            else
+                return 8;
+
+            start += (int) count;
+        }
+
+        //Returns 6 if the number of cells does not match the grid width.
+        if(start != grid.width)
+        {
+            return 6;
+        }
+        //Reads the next line.
+        fgets(buffer, BUFFER_SIZE, file);
+    }
+
+    //Returns 9 if there is an error setting the initial values for the neighbours.
+    if(setInitialNeighbours() == 1)
+        return 9;
+
+    return 0;
 }
--- fileHandler.h
+++ fileHandler.h
@@ -1,11 +1,18 @@
 #ifndef GAMEOFLIFE_FILEHANDLER_H
 #define GAMEOFLIFE_FILEHANDLER_H
 #include <stdio.h>
+#include "logic.h"
 
+//The format of the txt file generated follows a similar format to the .rle format found on the conway life wiki.
+//https://www.conwaylife.com/wiki/Run_Length_Encoded
 //Writes the final state of the game of life into a file.
 int writeState(FILE *file);
 
 //Reads the initial state of the game of life into the program.
 int readState(FILE *file);
 
+extern int iterations;
+extern int tickRate;
+static const int BUFFER_SIZE = 200;
+static const char* GAMEOFLIFE_FORMAT = "%u%[^0123456789]%n";
 #endif //GAMEOFLIFE_FILEHANDLER_H
