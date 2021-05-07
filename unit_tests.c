b32eb044c4336f59595bbddcb0f8c070c421a1b9
Daniel Phan <sc20dkvp@leeds.ac.uk>
2021-05-07 02:34:57 +0100

    First unit test written.

--- unit_tests.c
+++ unit_tests.c
@@ -1,7 +1,51 @@
 #include "unity.h"
 #include "logic.h"
 #include "fileHandler.h"
-#include "graphics.h"
+
+void test_create_grid()
+{
+    int x = 0;
+    int y = 0;
+    int test = createGrid(x, y);
+    TEST_ASSERT_MESSAGE(test == 2, "Failure");
+
+    //Test edge cases
+    x = 40;
+    y = 10;
+    test = createGrid(x, y);
+    TEST_ASSERT_MESSAGE(test == 0, "Fails on edge cases!\n");
+
+}
+
+void test_set_next_state()
+{
+
+}
+
+void test_set_initial_state()
+{
+
+}
+
+void test_check_neighbours()
+{
+
+}
+
+void test_write_grid()
+{
+
+}
+
+void test_write_state()
+{
+
+}
+
+void test_read_state()
+{
+
+}
 
 void setUp()
 {
@@ -14,7 +58,12 @@ void tearDown()
 
 int main() {
     UNITY_BEGIN();
-    RUN_TEST();
-
+    RUN_TEST(test_create_grid);
+    RUN_TEST(test_set_next_state);
+    RUN_TEST(test_set_initial_state);
+    RUN_TEST(test_check_neighbours);
+    RUN_TEST(test_write_grid);
+    RUN_TEST(test_write_state);
+    RUN_TEST(test_read_state);
     return UNITY_END();
 }
